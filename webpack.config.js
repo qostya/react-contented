29eb82343903f061fad5d4cc487b40b2f5427e90
Constantine Beldy <qostyab@gmail.com>
2017-05-07 23:37:28 +0300

    package.json updated

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: ["whatwg-fetch", "./src/index.js"],
+  output: {
+    path: __dirname,
+    filename: "./dist/frontend.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "sass-loader" // compiles Sass to CSS
+        }]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'es2015']
+          }
+        }
+      }
+    ]
+  },
+  // npm run build:prod || webpack -p
+  plugins: process.env.NODE_ENV === 'production' ? [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ] : []
+};
